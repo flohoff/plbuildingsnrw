d584c74f7cd13840bdbaeb539dbf0bda7ed1aa55
Florian Lohoff <f@zz.de>
2022-10-27 10:28:53 +0200

    Use the gfk (Gebäudefunktion) to add additional tags
    building=shed
    building=garage
    building=yes + power=substation

--- MultiPolygon.pm
+++ MultiPolygon.pm
@@ -7,17 +7,17 @@ package MultiPolygon;
 	use Geo::WKT;
 
 	sub new {
-		my ($class, $string) = @_;
+		my ($class, $row) = @_;
 
 		my $self={
-			string => $string,
+			row => $row,
 			nid => -1,
 			nodes => {},
-			parsed => from_json($string),
+			parsed => from_json($row->{geojson}),
 		};
 		bless $self, $class;
 
-		$self->_parse($string);
+		$self->_parse($row->{geojson});
 
 		return $self;
 	}
@@ -63,6 +63,33 @@ package MultiPolygon;
 		#printf("Outer: %d\n", scalar @{$mp});
 	}
 
+	my $funktion2type={
+		'0' => { 'building' => 'yes' },
+		'2463' => { 'building' => 'garage' },
+		'2523' => { 'building' => 'yes', 'power' => 'substation' },
+		'2723' => { 'building' => 'shed' }
+	};
+
+	sub tags {
+		my ($self) = @_;
+
+		my $gfk=$self->{row}{gfk};
+		my ($dummy, $funktion) = split(/_/, $gfk);
+
+		my $t=$funktion2type->{$funktion};
+		if (!defined($t)) {
+			$t=$funktion2type->{0};
+		}
+
+		my @tags;
+		foreach my $k ( keys %{$t} ) {
+			push @tags, sprintf("\t<tag k=\"%s\" v=\"%s\"/>",
+					$k, $t->{$k});
+		}
+
+		return @tags;
+	}
+
 	sub osmxml {
 		my ($self) = @_;
 
@@ -75,13 +102,10 @@ package MultiPolygon;
 			my @nodes=map {
 				sprintf("\t<nd ref=\"%s\"/>", $_);
 			} @{$_};
-			my @tags=(
-				"\t<tag k=\"building\" v=\"house\"/>"
-			);
 			sprintf("<way id=\"%s\">\n%s\n%s\n</way>\n",
 				$self->{nid}--,
 				join("\n", @nodes),
-				join("\n", @tags)
+				join("\n", $self->tags())
 				);
 		} @{$self->{rings}};
 
